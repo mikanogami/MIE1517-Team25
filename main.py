505dc5c536f883df741b1fe5fa65caab6cc0e0b6
Mika Nogami <mikanogami@Mikas-MacBook-Pro-4.local>
2025-03-25 11:55:14 -0400

    resolves git conflicts

--- main.py
+++ main.py
@@ -119,7 +119,6 @@ def run_sim():
             # TODO: once expert is implemented
             WALL_DISTANCE = 2.2   # 2
 
-<<<<<<< Updated upstream
         if True in keypress:
             ROBOT.move_constant_speed_manual([*BLOCK.block_square, *MAZE.reduced_walls], keypress)
         # if u1 < 8 and u2 < 4:  # Obstacle detected in front - 6
@@ -134,22 +133,6 @@ def run_sim():
             error = WALL_DISTANCE - u0
             steering_adjustment = EXPERT.compute(error)
             ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=steering_adjustment)
-=======
-            if True in keypress:
-                ROBOT.move_constant_speed_manual([*BLOCK.block_square, *MAZE.reduced_walls], keypress)
-            # if u1 < 8 and u2 < 4:  # Obstacle detected in front - 6
-            #     print("Wall ahead! Turning left...")
-            #     ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=200)
-                # print("detect")
-            elif u1 < 8:  # Obstacle detected in front - 6
-                print("Wall ahead! Turning left...")
-                ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=5)
-            else:
-                # Follow right wall using PID (EXPERT control branch, do not modify this)
-                error = WALL_DISTANCE - u0
-                steering_adjustment = EXPERT.compute(error)
-                ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=steering_adjustment)
->>>>>>> Stashed changes
 
                 # Log training data for expert control: timestamp, sensor readings, and steering adjustment
                 timestamp = datetime.datetime.now().isoformat()
