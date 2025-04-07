2e50faa785b0c06f14782801db86e9ebeb238e88
Mika Nogami <mikanogami@mac.home>
2025-04-06 21:04:53 -0400

    made modifications to simulator so that we can run sim from ipynb

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ import utilities
 from expert import Expert
 from net import RobotControlNet
 
-def run_sim(model=None, dagger_itr=None, runtime=None, save_data_dir=None):
+def run_sim(model=None, dagger_itr=None, runtime=None, save_data_dir=None, start_pos=None, start_rot=None, is_clockwise=True):
     ### Initialization
     print('SimMeR Loading...')
 
@@ -55,6 +55,10 @@ def run_sim(model=None, dagger_itr=None, runtime=None, save_data_dir=None):
 
     # Load robot
     ROBOT = Robot()
+    if start_pos is not None:
+        ROBOT.position = start_pos
+    if start_rot is not None:
+        ROBOT.rotation = start_rot
 
     # List of sensors to simulate every frame (for testing only)
     if hasattr(CONFIG, 'simulate_list'):
@@ -96,113 +100,115 @@ def run_sim(model=None, dagger_itr=None, runtime=None, save_data_dir=None):
 
     previous_adjustment = 0
 
-    try:
-        while RUNNING:
+    while RUNNING:
 
-            ##########################
-            ##### USER INTERFACE #####
-            ##########################
-            # Check for keyboard input
-            game_events = pygame.event.get()
-            RUNNING = HUD.check_input(game_events)
-            keypress = pygame.key.get_pressed()
+        game_events = pygame.event.get()
+        for event in game_events:
+            if event.type == pygame.QUIT:
+                RUNNING = False
 
-            # Manually simulate a specific sensor or sensors
-            #utilities.simulate_sensors(environment, SIMULATE_LIST)
-             
-            u0 = ROBOT.sensors.get('u0').simulate(0, environment)   # left-facing, left sensor reading
-            u1 = ROBOT.sensors.get('u1').simulate(0, environment)   # forward-facing, left sensor reading 
-            u2 = ROBOT.sensors.get('u2').simulate(0, environment)   # forward-facing, middle sensor reading
-            u3 = ROBOT.sensors.get('u3').simulate(0, environment)   # forward-facing, right sensor reading
-            u4 = ROBOT.sensors.get('u4').simulate(0, environment)   # right-facing, right sensor reading
+        RUNNING = HUD.check_input(game_events)
 
-            if expert_bool:
-                # Use both track and trajectory error to adjust steering angle
-                cte, heading_error = EXPERT.get_cte(ROBOT.position, ROBOT.rotation, CW=CONFIG.clockwise)
-                track_adjustment = TRACK_PID.compute(cte)
-                trajectory_adjustment = TRAJECTORY_PID.compute(heading_error)
-                steering_adjustment =  track_adjustment + trajectory_adjustment
+        keypress = pygame.key.get_pressed()
 
-                alpha = 0.3  # Higher alpha means smoother but slower response
-                smoothed_adjustment = alpha * previous_adjustment + (1 - alpha) * steering_adjustment
-                previous_adjustment = smoothed_adjustment
-
-                # set bound on steering adjustment (required for our classification model)
-                if smoothed_adjustment > 1.5:
-                    smoothed_adjustment = 1.5
-                elif smoothed_adjustment < -1.5:
-                    smoothed_adjustment = -1.5
-                ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=smoothed_adjustment)
-                training_data.append((u3, u0, u1, u2, u4, smoothed_adjustment))
-
-            else:
-                sensor_vals = np.array([u0, u1, u2, u3, u4])
-                steering_adjustment = model.get_steering_cmd(sensor_vals)
-                ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=steering_adjustment)
-
-            # Recalculate global positions of the robot and its devices
-            ROBOT.update_outline()
-            ROBOT.update_device_positions()
-
-            cte, heading_error = EXPERT.get_cte(ROBOT.position, ROBOT.rotation, CW=CONFIG.clockwise)
-            # if cte or heading_error are too high, expert takes over
-            if not expert_bool and (abs(cte) > 1.0 or abs(heading_error) > 15):
-                print(f"{'CW' if CONFIG.clockwise else 'CCW'} - cte: {cte}, rot_error: {heading_error}")
-                expert_bool = True
-            elif model is not None and expert_bool:
-                if EXPERT.is_straight_section(ROBOT.position) and (abs(cte) < 0.5 or abs(heading_error) < 5):
-                    expert_bool = False
-                elif not EXPERT.is_straight_section(ROBOT.position) and (abs(cte) < 0.5 and abs(heading_error) < 5):
-                    expert_bool = False
+        # Manually simulate a specific sensor or sensors
+        #utilities.simulate_sensors(environment, SIMULATE_LIST)
             
-            ###########################################
-            ##### DRAW RELEVANT OBJECTS ON CANVAS #####
-            ###########################################
-            # Fill the background with the background color
-            canvas.fill(CONFIG.background_color)
+        u0 = ROBOT.sensors.get('u0').simulate(0, environment)   # left-facing, left sensor reading
+        u1 = ROBOT.sensors.get('u1').simulate(0, environment)   # forward-facing, left sensor reading 
+        u2 = ROBOT.sensors.get('u2').simulate(0, environment)   # forward-facing, middle sensor reading
+        u3 = ROBOT.sensors.get('u3').simulate(0, environment)   # forward-facing, right sensor reading
+        u4 = ROBOT.sensors.get('u4').simulate(0, environment)   # right-facing, right sensor reading
 
-            # Draw the maze checkerboard pattern
-            MAZE.draw_floor(canvas)
+        if expert_bool:
+            # Use both track and trajectory error to adjust steering angle
+            cte, heading_error = EXPERT.get_cte(ROBOT.position, ROBOT.rotation, CW=is_clockwise)
+            track_adjustment = TRACK_PID.compute(cte)
+            trajectory_adjustment = TRAJECTORY_PID.compute(heading_error)
+            steering_adjustment =  track_adjustment + trajectory_adjustment
 
-            # Draw the maze walls
-            MAZE.draw_walls(canvas)
+            alpha = 0.3  # Higher alpha means smoother but slower response
+            smoothed_adjustment = alpha * previous_adjustment + (1 - alpha) * steering_adjustment
+            previous_adjustment = smoothed_adjustment
 
-            # Draw the block
-            BLOCK.draw(canvas)
+            # set bound on steering adjustment (required for our classification model)
+            if smoothed_adjustment > 1.5:
+                smoothed_adjustment = 1.5
+            elif smoothed_adjustment < -1.5:
+                smoothed_adjustment = -1.5
+            ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=smoothed_adjustment)
+            training_data.append((u3, u0, u1, u2, u4, smoothed_adjustment))
 
-            # Draw the robot onto the maze
-            ROBOT.draw(canvas)
-            ROBOT.draw_devices(canvas)
+        else:
+            sensor_vals = np.array([u0, u1, u2, u3, u4])
+            steering_adjustment = model.get_steering_cmd(sensor_vals)
+            ROBOT.move_constant_speed(walls=[*BLOCK.block_square, *MAZE.reduced_walls], steering_angle=steering_adjustment)
 
-            # Update the various HUD elements
-            HUD.draw_frame_indicator(canvas)
-            HUD.draw_expert_indicator(canvas, expert_bool)
+        # Recalculate global positions of the robot and its devices
+        ROBOT.update_outline()
+        ROBOT.update_device_positions()
 
-            # Limit the framerate
-            HUD.clock.tick(CONFIG.frame_rate)
+        cte, heading_error = EXPERT.get_cte(ROBOT.position, ROBOT.rotation, CW=is_clockwise)
+        # if cte or heading_error are too high, expert takes over
+        if not expert_bool and (abs(cte) > 1.0 or abs(heading_error) > 15):
+            print(f"{'CW' if is_clockwise else 'CCW'} - cte: {cte}, rot_error: {heading_error}")
+            expert_bool = True
+        elif model is not None and expert_bool:
+            if EXPERT.is_straight_section(ROBOT.position) and (abs(cte) < 0.5 or abs(heading_error) < 5):
+                expert_bool = False
+            elif not EXPERT.is_straight_section(ROBOT.position) and (abs(cte) < 0.5 and abs(heading_error) < 5):
+                expert_bool = False
+        
+        ###########################################
+        ##### DRAW RELEVANT OBJECTS ON CANVAS #####
+        ###########################################
+        # Fill the background with the background color
+        canvas.fill(CONFIG.background_color)
 
-            # Flip the display (update the canvas)
-            pygame.display.flip()
+        # Draw the maze checkerboard pattern
+        MAZE.draw_floor(canvas)
 
-            # check that we have not surpassed runtime
-            if time.time() - start_time > runtime:
-                if model is None:
-                    RUNNING = False
-                elif not expert_bool:
-                    RUNNING = False
+        # Draw the maze walls
+        MAZE.draw_walls(canvas)
 
-    except KeyboardInterrupt:
-        return
+        # Draw the block
+        BLOCK.draw(canvas)
+
+        # Draw the robot onto the maze
+        ROBOT.draw(canvas)
+        ROBOT.draw_devices(canvas)
+
+        # Update the various HUD elements
+        HUD.draw_frame_indicator(canvas)
+        HUD.draw_expert_indicator(canvas, expert_bool)
+
+        # Limit the framerate
+        HUD.clock.tick(CONFIG.frame_rate)
+
+        # Flip the display (update the canvas)
+        pygame.display.flip()
+        pygame.display.update()
+
+        # check that we have not surpassed runtime
+        if time.time() - start_time > runtime:
+            if model is None:
+                RUNNING = False
+            elif not expert_bool:
+                RUNNING = False
     
     print(time.time() - start_time)
     # Save training data on exit
     if save_data_dir is not None:
-        with open(f"{save_data_dir}/training_data_{dagger_itr}_{'CW' if CONFIG.clockwise else 'CCW'}.csv", "w", newline="") as csvfile:
+        with open(f"{save_data_dir}/training_data_{dagger_itr}_{'CW' if is_clockwise else 'CCW'}.csv", "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(training_data)
             print('Saved CSV!')
+    
+    pygame.quit()
+    return
 
 if __name__ == "__main__":
+
     n_classes = 16
     n_sensors = 5
     batch_size = 256
@@ -216,7 +222,7 @@ if __name__ == "__main__":
     CONFIG.robot_start_position = start_pos_CW
     CONFIG.robot_start_rotation = start_rot_CW
     CONFIG.clockwise = True
-    run_sim(model=agent, save_data_dir=None, dagger_itr=3, runtime=120)
+    run_sim(CONFIG, model=agent, save_data_dir=None, dagger_itr=3, runtime=120)
 
     # run simulation with robot moving counter clockwise
     start_pos_CCW = [6, 12]
@@ -224,7 +230,7 @@ if __name__ == "__main__":
     CONFIG.robot_start_position = start_pos_CCW
     CONFIG.robot_start_rotation = start_rot_CCW
     CONFIG.clockwise = False
-    run_sim(model=agent, save_data_dir=None, dagger_itr=3, runtime=120)
+    run_sim(CONFIG, model=agent, save_data_dir=None, dagger_itr=3, runtime=120)
 
     print('Execution finished. Closing SimMeR.')
     pygame.quit()
